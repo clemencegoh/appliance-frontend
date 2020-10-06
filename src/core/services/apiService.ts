3bdadca9003c575df60b5699d067954c9583102b
Clemence Goh <clemencegoh@users.noreply.github.com>
2020-10-06 20:20:21 +0800

    workaround

--- src/core/services/apiService.ts
+++ src/core/services/apiService.ts
@@ -2,7 +2,8 @@ import axios, { AxiosError, AxiosResponse } from "axios";
 import { config } from "rxjs";
 
 const api = axios.create({
-  baseURL: "https://ec2-13-212-34-182.ap-southeast-1.compute.amazonaws.com",
+  baseURL:
+    "https://cors-anywhere.herokuapp.com/ec2-13-212-34-182.ap-southeast-1.compute.amazonaws.com",
   timeout: 5000,
 });
 
